a70eba9504bb492ef305a9b4edef355800f6d4b8
Plug-On <plugon035@gmail.com>
2025-04-13 08:02:40 +0545

    cart related

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -3,33 +3,44 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cart;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
-use App\Models\Cart;
 
 class CartController extends Controller
 {
     public function index()
     {
-        $carts = Cart::where('user-id', Auth::id())->with('project')->get();
+        $carts = Cart::where('user_id', Auth::id())->with('product')->get();
         return response()->json([
-            'status'=>200,
-            'carts'=>$carts,
+            'status' => 200,
+            'carts' => $carts,
         ]);
     }
 
     public function store(Request $request)
     {
-        $data = $request -> validate([
-            'product_id'=> 'required',
-            'qty'=> 'required'
+        $data = $request->validate([
+            'product_id' => 'required',
+            'qty' => 'required'
         ]);
+
+        $data['user_id'] = Auth::id();
+
+        $check = Cart::where('user_id', $data['user_id'])
+            ->where('product_id', $data['product_id'])
+            ->first();
+        if ($check) {
+            return response()->json([
+                'status' => 409, // Conflict
+                'message' => 'Product is already in cart',
+            ]);
+        }
 
         Cart::create($data);
         return response()->json([
-            'status'=> 200,
-            'message'=>'Product  added to cart successfully',
+            'status' => 200,
+            'message' => 'Product added to cart successfully',
         ]);
     }
 }
